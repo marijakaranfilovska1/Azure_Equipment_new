d01223684b3dfa7a879fd05db4c369764e23bb27
Marija Karanfilovska <marija.karanfilovska@interworks.com.mk>
2018-08-25 18:29:51 +0200

    Add project files.

---
+++ Core/Repository/EquipmentRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Core.Repository
+{
+    public class EquipmentRepository:IEquipmentRepository
+    {
+        public List<Equipment> GetEquipment()
+        {
+            List<Equipment> listEquipments = new List<Equipment>()
+            {
+                new Equipment()
+                {
+                    IdEquipment = 1,
+                    Outlet = "11",
+                    SerialNumber = "111",
+                    Model = "Bc",
+                    MacAddress = "369785"
+                },
+                new Equipment()
+                {
+                    IdEquipment = 2,
+                    Outlet = "Data",
+                    SerialNumber = "222",
+                    Model = "Dt",
+                    MacAddress = "9654785"
+                }
+            };
+
+            return listEquipments;
+        }
+    }
+}
